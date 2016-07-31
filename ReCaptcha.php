41374c52405a5eddcff9d48146f5193e3317245f
AlikDex <alikdexor@gmail.com>
2016-07-31 21:30:27 +0700

    Update ReCaptcha.php

--- ReCaptcha.php
+++ ReCaptcha.php
@@ -8,7 +8,10 @@ namespace alikdex\recaptcha;
 
 use Yii;
 use yii\base\InvalidConfigException;
+
 use yii\helpers\Html;
+use yii\helpers\ArrayHelper;
+
 use yii\widgets\InputWidget;
 /**
  * Yii2 Google reCAPTCHA widget.
@@ -38,10 +41,192 @@ use yii\widgets\InputWidget;
  */
 class ReCaptcha extends InputWidget
 {
+	const JS_API_URL = '//www.google.com/recaptcha/api.js';
 
+	const THEME_LIGHT = 'light';
+	const THEME_DARK = 'dark';
 
-	public function run()
+	const TYPE_IMAGE = 'image';
+	const TYPE_AUDIO = 'audio';
+
+	const SIZE_NORMAL = 'normal';
+	const SIZE_COMPACT = 'compact';
+
+	/**
+	 *	@var string validation input name.
+	 */
+	public $name = 'ReCaptcha';
+
+	/**
+	 *	@var string Your sitekey.
+	 */
+	public $siteKey;
+
+	/**
+	 *	@var string Your secret.
+	 */
+	public $secret;
+
+    /**
+     *	@var string The color theme of the widget. [[THEME_LIGHT]] (default) or [[THEME_DARK]]
+     */
+    public $theme;
+
+    /**
+     *	@var string The type of CAPTCHA to serve. [[TYPE_IMAGE]] (default) or [[TYPE_AUDIO]]
+     */
+    public $type;
+
+    /**
+     *	@var string The size of the widget. [[SIZE_NORMAL]] (default) or [[SIZE_COMPACT]]
+     */
+    public $size;
+
+    /**
+     *	@var int The tabindex of the widget
+     */
+    public $tabindex;
+
+    /**
+     *	@var string Your JS callback function that's executed when the user submits a successful CAPTCHA response.
+     */
+    public $jsCallback;
+
+    /**
+     *	@var string Your JS callback function that's executed when the recaptcha response expires and the user needs to solve a new CAPTCHA.
+     */
+    public $jsExpiredCallback;
+
+    /**
+     *	@var array Additional html widget options, such as `class`.
+     */
+    public $widgetOptions = [];
+
+    /**
+     *	@var array Additional html widget options, such as `class`.
+     */
+    public $recaptchaOptions = [];
+
+    /**
+     * @throws \yii\base\InvalidConfigException
+     */
+	public function init()
 	{
-		echo 'recaptcha';
+		parent::init();
+
+		if (is_null($this->siteKey)) {
+			if (!is_null(Yii::$app->reCaptcha->siteKey)) {
+				$this->siteKey = Yii::$app->reCaptcha->siteKey;
+			} else {
+				throw new InvalidConfigException('Required `siteKey` param isn\'t set.');
+			}
+		}
+	}
+
+    public function run()
+    {
+		$this->registerClientScript();
+
+		$this->customFieldPrepare();
+
+		$divOptions = [
+			'class' => 'g-recaptcha',
+			'data-sitekey' => $this->siteKey
+		];
+
+		if (!empty($this->jsCallback)) {
+			$divOptions['data-callback'] = $this->jsCallback;
+		}
+		if (!empty($this->jsExpiredCallback)) {
+			$divOptions['data-expired-callback'] = $this->jsExpiredCallback;
+		}
+		if (!empty($this->theme)) {
+			$divOptions['data-theme'] = $this->theme;
+		}
+		if (!empty($this->type)) {
+			$divOptions['data-type'] = $this->type;
+		}
+		if (!empty($this->size)) {
+			$divOptions['data-size'] = $this->size;
+		}
+		if (!empty($this->tabindex)) {
+			$divOptions['data-tabindex'] = $this->tabindex;
+		}
+
+		if (isset($this->widgetOptions['class'])) {
+			Html::addCssClass($divOptions, $this->widgetOptions);
+		}
+
+		$divOptions = $divOptions + $this->widgetOptions;
+
+		echo Html::tag('div', '', $divOptions);
+    }
+
+    /**
+     * Registers required script for the plugin to work as jQuery File Uploader
+     */
+    public function registerClientScript()
+    {
+		$this->getView()->registerJsFile(
+			self::JS_API_URL . '?hl=' . $this->getLanguageSuffix(),
+			[
+				'position' => \yii\web\View::POS_HEAD,
+				'async' => true,
+				'defer' => true
+			]
+		);
+    }
+
+	/**
+	 *	Check application language
+	 *	@return string
+	 */
+	protected function getLanguageSuffix()
+	{
+		$currentAppLanguage = Yii::$app->language;
+		$langsExceptions = ['zh-CN', 'zh-TW', 'zh-TW'];
+
+		if (strpos($currentAppLanguage, '-') === false) {
+			return $currentAppLanguage;
+		}
+
+		if (in_array($currentAppLanguage, $langsExceptions)) {
+			return $currentAppLanguage;
+		} else {
+			return substr($currentAppLanguage, 0, strpos($currentAppLanguage, '-'));
+		}
+	}
+
+	protected function customFieldPrepare()
+	{
+		$view = $this->view;
+
+		if ($this->hasModel()) {
+			$inputName = Html::getInputName($this->model, $this->attribute);
+			$inputId = Html::getInputId($this->model, $this->attribute);
+		} else {
+			$inputName = $this->name;
+			$inputId = 'recaptcha-' . $this->name;
+		}
+
+		if (empty($this->jsCallback)) {
+			$jsCode = "var recaptchaCallback = function(response){jQuery('#{$inputId}').val(response);};";
+		} else {
+			$jsCode = "var recaptchaCallback = function(response){jQuery('#{$inputId}').val(response); {$this->jsCallback}(response);};";
+		}
+
+		$this->jsCallback = 'recaptchaCallback';
+
+		if (empty($this->jsExpiredCallback)) {
+			$jsExpCode = "var recaptchaExpiredCallback = function(){jQuery('#{$inputId}').val('');};";
+		} else {
+			$jsExpCode = "var recaptchaExpiredCallback = function(){jQuery('#{$inputId}').val(''); {$this->jsExpiredCallback}(response);};";
+		}
+
+		$this->jsExpiredCallback = 'recaptchaExpiredCallback';
+		$view->registerJs($jsCode, $view::POS_BEGIN);
+		$view->registerJs($jsExpCode, $view::POS_BEGIN);
+
+		echo Html::input('hidden', $inputName, null, ['id' => $inputId]);
 	}
 }
