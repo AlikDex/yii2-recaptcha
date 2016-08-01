2e10e9939ac81c47cc01ab0b35da561f60457101
AlikDex <alikdexor@gmail.com>
2016-08-01 13:51:03 +0700

    Update ReCaptchaValidator.php

--- ReCaptchaValidator.php
+++ ReCaptchaValidator.php
@@ -11,53 +11,62 @@ use yii\base\InvalidConfigException;
 use yii\helpers\Json;
 use yii\validators\Validator;
 /**
- * ReCaptcha widget validator.
+ *	ReCaptcha widget validator.
  *
- * @author AliKDex
- * @package alikdex\recaptcha
+ *	@author AliKDex
+ *	@package alikdex\recaptcha
  */
 class ReCaptchaValidator extends Validator
 {
-    const GRABBER_PHP = 1; // file_get_contents
-    const GRABBER_CURL = 2; // CURL, because sometimes file_get_contents is deprecated
-    const SITE_VERIFY_URL = 'https://www.google.com/recaptcha/api/siteverify';
-    const CAPTCHA_RESPONSE_FIELD = 'g-recaptcha-response';
-    /** @var boolean Whether to skip this validator if the input is empty. */
-    public $skipOnEmpty = false;
-    /** @var string The shared key between your site and ReCAPTCHA. */
-    public $secret;
-    /** @var int Choose your grabber for getting JSON, self::GRABBER_PHP = file_get_contents, self::GRABBER_CURL = CURL */
-    public $grabberType = self::GRABBER_PHP;
-    public $uncheckedMessage;
-    public function init()
-    {
-        parent::init();
-        if (empty($this->secret)) {
-            if (!empty(Yii::$app->reCaptcha->secret)) {
-                $this->secret = Yii::$app->reCaptcha->secret;
-            } else {
-                throw new InvalidConfigException('Required `secret` param isn\'t set.');
-            }
-        }
-        if ($this->message === null) {
-            $this->message = Yii::t('yii', 'The verification code is incorrect.');
-        }
+	const SITE_VERIFY_URL = 'https://www.google.com/recaptcha/api/siteverify';
+	const CAPTCHA_RESPONSE_FIELD = 'g-recaptcha-response';
+
+	/**
+	 *	@var boolean Whether to skip this validator if the input is empty.
+	 */
+	public $skipOnEmpty = false;
+
+	/**
+	 *	@var string The shared key between your site and ReCAPTCHA.
+	 */
+	public $secret;
+
+	public $uncheckedMessage;
+
+	public function init()
+	{
+		parent::init();
+
+		if (null === $this->secret) {
+			if (null !== Yii::$app->reCaptcha->secret) {
+				$this->secret = Yii::$app->reCaptcha->secret;
+			} else {
+				throw new InvalidConfigException('Required `secret` param isn\'t set.');
+			}
+		}
+
+		if ($this->message === null) {
+			$this->message = Yii::t('yii', 'The verification code is incorrect.');
+		}
     }
-    /**
-     * @param \yii\base\Model $model
-     * @param string $attribute
-     * @param \yii\web\View $view
-     * @return string
-     */
-    public function clientValidateAttribute($model, $attribute, $view)
-    {
-        $message = $this->uncheckedMessage ? $this->uncheckedMessage : Yii::t(
-            'yii',
-            '{attribute} cannot be blank.',
-            ['attribute' => $model->getAttributeLabel($attribute)]
-        );
-        return "(function(messages){if(!grecaptcha.getResponse()){messages.push('{$message}');}})(messages);";
-    }
+
+	/**
+	 * @param \yii\base\Model $model
+	 * @param string $attribute
+	 * @param \yii\web\View $view
+	 * @return string
+	 */
+	public function clientValidateAttribute($model, $attribute, $view)
+	{
+		$message = $this->uncheckedMessage ? $this->uncheckedMessage : Yii::t(
+			'yii',
+			'{attribute} cannot be blank.',
+			['attribute' => $model->getAttributeLabel($attribute)]
+		);
+
+		return "(function(messages){if(!grecaptcha.getResponse()){messages.push('{$message}');}})(messages);";
+	}
+
     /**
      * @param string $value
      * @return array|null
@@ -70,56 +79,50 @@ class ReCaptchaValidator extends Validator
                 return [$this->message, []];
             }
         }
-        $request = self::SITE_VERIFY_URL . '?' . http_build_query([
-                'secret' => $this->secret,
-                'response' => $value,
-                'remoteip' => Yii::$app->request->userIP
-            ]);
-        $response = $this->getResponse($request);
+
+		$response = $this->_submitHttpGet([
+			'secret' => $this->secret,
+			'remoteip' => Yii::$app->request->userIP,
+			'response' => $value,
+		]);
+
+		if ($response === false)
+			throw new Exception('Unable connection to the captcha server.');
+		else
+			$response = Json::decode($response, true);
+
         if (!isset($response['success'])) {
             throw new Exception('Invalid recaptcha verify response.');
         }
+
         return $response['success'] ? null : [$this->message, []];
     }
-    /**
-     * @param string $request
-     * @return mixed
-     * @throws Exception
-     */
-    protected function getResponse($request)
-    {
-        if ($this->grabberType === self::GRABBER_PHP) {
-            $response = @file_get_contents($request);
-            if ($response === false) {
-                throw new Exception('Unable connection to the captcha server.');
-            }
-        } else {
-            $options = array(
-                CURLOPT_CUSTOMREQUEST => 'GET',     //set request type post or get
-                CURLOPT_POST => false,              //set to GET
-                CURLOPT_RETURNTRANSFER => true,     // return web page
-                CURLOPT_HEADER => false,            // don't return headers
-                CURLOPT_FOLLOWLOCATION => true,     // follow redirects
-                CURLOPT_ENCODING => '',             // handle all encodings
-                CURLOPT_AUTOREFERER => true,        // set referer on redirect
-                CURLOPT_CONNECTTIMEOUT => 120,      // timeout on connect
-                CURLOPT_TIMEOUT => 120,             // timeout on response
-                CURLOPT_MAXREDIRS => 10,            // stop after 10 redirects
-            );
-            $ch = curl_init($request);
-            curl_setopt_array($ch, $options);
-            $content = curl_exec($ch);
-            $err = curl_errno($ch);
-            $errmsg = curl_error($ch);
-            $header = curl_getinfo($ch);
-            curl_close($ch);
-            $header['errno'] = $err;
-            $header['errmsg'] = $errmsg;
-            $response = $content;
-            if ($header['errno'] !== 0) {
-                throw new Exception('Unable connection to the captcha server. ' . $header['errmsg']);
-            }
-        }
-        return Json::decode($response, true);
-    }
+
+	/**
+	 * HTTP GET to communicate with reCAPTCHA server
+	 * @param array $data Array of params
+	 * @return string JSON response from reCAPTCHA server
+	 */
+	private function _submitHTTPGet($data)
+	{
+		/**
+		 * PHP 5.6.0 changed the way you specify the peer name for SSL context options.
+		 * Using "CN_name" will still work, but it will raise deprecated errors.
+		 */
+		$peer_key = version_compare(PHP_VERSION, '5.6.0', '<') ? 'CN_name' : 'peer_name';
+		$options = [
+			'http' => [
+				'header' => "Content-type: application/x-www-form-urlencoded\r\n",
+				'method' => 'POST',
+				'content' => http_build_query($data, '', '&'),
+					// Force the peer to validate (not needed in 5.6.0+, but still works
+				'verify_peer' => true,
+					// Force the peer validation to use www.google.com
+				$peer_key => 'www.google.com',
+			],
+		];
+
+		$context = stream_context_create($options);
+		return file_get_contents(self::SITE_VERIFY_URL, false, $context);
+	}
 }
